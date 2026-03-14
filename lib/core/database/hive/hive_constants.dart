ccd99e4c885199e66b02ab1d94c32ade0a129bfb
abdulmas01 <auwalabdul2015@gmail.com>
2026-03-14 13:50:48 +0100

    remove redundant fiells in hive constant

--- lib/core/database/hive/hive_constants.dart
+++ lib/core/database/hive/hive_constants.dart
@@ -10,27 +10,6 @@ class HiveConstants {
   static const deviceIdKey = "deviceId";
   static const deviceUserIdKey = "deviceUserId";
   static const deviceUserEmailKey = "deviceUserEmail";
-
-  static const String walletBox = "walletBox";
-  static const String walletKey = "wallet";
-  static const String hideBalanceKey = "hideBalance";
-  static const String subscriptionBox = "subscriptionBox";
-  static const String subscriptionSummaryKey = "subscriptionSummary";
-  static const String chatMessagesBox = "chat_messages_box";
-
-  static const String signalIdentityBox = "signal_identity_box";
-  static const String signalPreKeyBox = "signal_prekey_box";
-  static const String signalSignedPreKeyBox = "signal_signed_prekey_box";
-  static const String signalSessionBox = "signal_session_box";
-  static const String signalMetaBox = "signal_meta_box";
-  static const String signalUserKeysBox = "signal_user_keys_box";
-
-  static const String identityKeyPairKey = "identity_key_pair";
-  static const String registrationIdKey = "registration_id";
-  // TODO : comapare with this chatMessagesBox to make sure everything is working
-  // and we dont have dublicate stuff
-  static const String messagesBoxName = 'chat_messages';
-  static const String sessionsBoxName = 'chat_sessions';
   static const String settingsBox = "settings_box";
   static const String settingsKey = "app_settings";
 }
